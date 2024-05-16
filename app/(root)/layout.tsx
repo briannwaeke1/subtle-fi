e5b1772b0e1a2174cf358869e0e5ee8fe2db7d3e
Brian Nwaeke <94656081+briannwaeke1@users.noreply.github.com>
2024-05-16 04:30:41 -0700

    implement side and mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,38 @@
+import MobileNav from "@/components/MobileNav";
+import SiderBar from "@/components/SiderBar";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn: User = {
+    firstName: "Brian",
+    lastName: "Nwaeke",
+    $id: "",
+    email: "",
+    userId: "",
+    dwollaCustomerUrl: "",
+    dwollaCustomerId: "",
+    address1: "",
+    city: "",
+    state: "",
+    postalCode: "",
+    dateOfBirth: "",
+    ssn: "",
+  }; // replace with actual user object
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SiderBar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
---
+++ components/SiderBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const SiderBar = ({ user }: SidebarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            alt="subtleTech logo"
+            width={34}
+            height={34}
+            className="size-[24px]
+            max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">SubtleTech</h1>
+        </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", {
+                "bg-bank-gradient": isActive,
+              })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
+      </nav>
+      FOOTER
+    </section>
+  );
+};
+
+export default SiderBar;
