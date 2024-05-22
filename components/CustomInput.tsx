777809dbbd67e9ad6be4b153d94a6f36bc38488d
Brian Nwaeke <94656081+briannwaeke1@users.noreply.github.com>
2024-05-22 09:03:09 -0700

    implement sign in and sign up form components, need to hook up to appwrite for auth

---
+++ components/CustomInput.tsx
@@ -0,0 +1,52 @@
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authFormSchema } from "@/lib/utils";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = authFormSchema("sign-up");
+
+interface CustomInputProps {
+  label: string;
+  name: FieldPath<z.infer<typeof formSchema>>;
+  placeholder: string;
+  control: Control<z.infer<typeof formSchema>>;
+}
+
+const CustomInput = ({
+  label,
+  name,
+  placeholder,
+  control,
+}: CustomInputProps) => {
+  return (
+    <div>
+      <FormField
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <div className="form-item">
+            <FormLabel className="form-label">{label}</FormLabel>
+            <div className="flex w-full flex-col">
+              <FormControl>
+                <Input
+                  placeholder={placeholder}
+                  className="input-class"
+                  type={name === "password" ? "password" : "text"}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="form-message" />
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+export default CustomInput;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-prototype-builtins */
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,24 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) =>
+  z.object({
+    // sign up
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+    postalCode:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dob: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(4),
+
+    // both
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  });
